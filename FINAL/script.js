f7a0686a21b03e1b295ffd212985e56fcd0325e1
Manuel Godoy <manuelgodoyvzla@gmail.com>
2023-05-20 00:23:30 +0200

    Commit

---
+++ FINAL/script.js
@@ -0,0 +1,5 @@
+document.getElementById('navbar-toggle-btn').addEventListener('click', function() {
+    var navbarMenu = document.getElementById('navbar-menu');
+    navbarMenu.style.display = (navbarMenu.style.display === 'none') ? 'block' : 'none';
+  });
+  
